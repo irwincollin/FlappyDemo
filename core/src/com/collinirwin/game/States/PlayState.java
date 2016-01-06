046a9341d2dc56d6d0d7cb62d2fbeca1e0507586
Collin Irwin <irwincollin+github@gmail.com>
2016-01-05 23:16:01 -0500

    Part  9 moving obstacles + bug fix
    Added limit to bird.java to prevent flying through game ceiling.
    
    Bird now moves through the world at MOVEMENT speed
    
    PlayState maintains tube array, renders tubes, and repositions them when
    they leave screen. Tube has new reposition method.

--- core/src/com/collinirwin/game/States/PlayState.java
+++ core/src/com/collinirwin/game/States/PlayState.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.collinirwin.game.FlappyDemo;
 import com.collinirwin.game.sprites.Bird;
 import com.collinirwin.game.sprites.Tube;
@@ -13,9 +14,14 @@ import com.collinirwin.game.sprites.Tube;
  * Created by irwin on 1/4/2016.
  */
 public class PlayState extends State {
+    private static final int TUBE_SPACING = 125;
+    private static final int TUBE_COUNT = 4;
+
     private Bird bird;
     private Texture bg;
     private Tube tube;
+
+    private Array<Tube> tubes;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
@@ -23,6 +29,12 @@ public class PlayState extends State {
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
         bg = new Texture("bg.png");
         tube = new Tube(100);
+
+        tubes = new Array<Tube>();
+
+        for(int i = 1; i <= TUBE_COUNT; i++) {
+            tubes.add(new Tube(i * (TUBE_SPACING + Tube.TUBE_WIDTH)));
+        }
     }
 
     @Override
@@ -36,6 +48,19 @@ public class PlayState extends State {
     public void update(float dt) {
         handleInput();
         bird.update(dt);
+        cam.position.x = bird.getPosition().x + 80;
+
+        for(Tube tube : tubes) {
+            final float camPos = cam.position.x - (cam.viewportWidth / 2.0f);
+            final float tubeEdge = tube.getPosTopTube().x + tube.getTopTube().getWidth();
+            final boolean isOffScreen = camPos > tubeEdge;
+            if (isOffScreen) {
+                final float newPos = tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT);
+                tube.reposition(newPos);
+            }
+        }
+
+        cam.update();
     }
 
     @Override
@@ -47,11 +72,12 @@ public class PlayState extends State {
         Vector3 birdPos = bird.getPosition();
         sb.draw(bird.getTexture(), birdPos.x, birdPos.y);
 
-        Vector2 topPos = tube.getPosTopTube();
-        sb.draw(tube.getTopTube(), topPos.x, topPos.y);
-
-        Vector2 botPos = tube.getPosBotTube();
-        sb.draw(tube.getBottomTube(), botPos.x, botPos.y);
+        for(Tube tube : tubes) {
+            Vector2 topPos = tube.getPosTopTube();
+            Vector2 botPos = tube.getPosBotTube();
+            sb.draw(tube.getTopTube(), topPos.x, topPos.y);
+            sb.draw(tube.getBottomTube(), botPos.x, botPos.y);
+        }
         sb.end();
     }
 
--- core/src/com/collinirwin/game/sprites/Bird.java
+++ core/src/com/collinirwin/game/sprites/Bird.java
@@ -2,12 +2,14 @@ package com.collinirwin.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector3;
+import com.collinirwin.game.FlappyDemo;
 
 /**
  * Created by irwin on 1/4/2016.
  */
 public class Bird {
     private static final int GRAVITY = -15;
+    private static final int MOVEMENT = 100;
 
     private Vector3 position;
     private Vector3 velocity;
@@ -25,11 +27,15 @@ public class Bird {
             velocity.add(0, GRAVITY, 0);
         }
         velocity.scl(dt);
+        position.add(MOVEMENT * dt, velocity.y, 0);
         position.add(0, velocity.y, 0);
         if(position.y < 0) {
             position.y = 0;
         }
-        velocity.scl(1/dt);
+        else if( position.y > (FlappyDemo.HEIGHT / 2) - 15) {
+            position.y = (FlappyDemo.HEIGHT / 2) - 15;
+        }
+        velocity.scl(1 / dt);
     }
 
     public Vector3 getPosition() {
